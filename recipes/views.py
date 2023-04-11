f34924a134925056945b89a39cfc447fb0fb3ff5
Vitor <vitorribeiro062@gmail.com>
2023-04-11 02:26:52 -0300

    Reorganização dos templates e separação da head

--- recipes/views.py
+++ recipes/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 def home(request):
     return render(
         request=request,
-        template_name='recipes/home.html',
+        template_name='recipes/pages/home.html',
         context={
             'name': 'Vitor'
         }
